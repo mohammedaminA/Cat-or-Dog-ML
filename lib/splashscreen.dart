91022e360be83ba442c31767b7a732cc8d7c75f6
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-02 17:39:03 +0300

    splashscreen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -5,8 +5,10 @@ class MySplash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text('Cat or Dog ML Classifier', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Color(0X00FFFF),),),
     );
   }
 }
