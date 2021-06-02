60a7beaf0752b7a3888cb641de4bca8ce8b2a3d2
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-02 17:34:38 +0300

    splashscreen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
